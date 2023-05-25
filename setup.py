837614b7bba8a0d8215d647d95a61ae4f9a45949
waby <wabyking@gmail.com>
2023-05-25 14:38:50 +0800

    Update setup.py
    hautuo support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='llmzoo',
-    version='0.1.7',    
+    version='0.1.8',    
     description='A example Python package',
     url='https://github.com/FreedomIntelligence/LLMZOO-API-SDK',
     author='Benyou Wang',
